b8fadd35c60a635088b814f1451b7c70a8d5eefd
Ahmedelsayed265 <ahmedelsayed2102@icloud.com>
2025-02-01 14:50:10 +0200

    refactor: enhance NotFound component with improved layout and styling; add navigation link to home

--- src/routes/NotFound.jsx
+++ src/routes/NotFound.jsx
@@ -1,3 +1,20 @@
+import { Link } from "react-router-dom";
+
 export default function NotFound() {
-  return <div>NotFound</div>;
+  return (
+    <section className="not_found_section">
+      <div className="container">
+        <h1>
+          404 - Oops Page
+          <br />
+          Not Found in Data.
+        </h1>
+        <p>
+          Oops! The web page you’re looking for doesn’t exist. Please click the
+          below button to get back to the home.
+        </p>
+        <Link to="/"> Back to Home </Link>
+      </div>
+    </section>
+  );
 }
